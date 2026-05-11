7f95c28f190d2bcf67b171e07162ad85f3add20a
darksaid98 <darksaid9889@gmail.com>
2026-05-11 22:19:55 +0200

    fix: exceptions serial uid compile warnings

--- common/src/main/java/io/github/exampleuser/example/config/exception/ConfigValidationException.java
+++ common/src/main/java/io/github/exampleuser/example/config/exception/ConfigValidationException.java
@@ -2,7 +2,11 @@ package io.github.exampleuser.example.config.exception;
 
 import org.spongepowered.configurate.serialize.SerializationException;
 
+import java.io.Serial;
+
 public class ConfigValidationException extends SerializationException {
+    private static @Serial final long serialVersionUID = 1L;
+
     public ConfigValidationException(String message) {
         super(message);
     }
